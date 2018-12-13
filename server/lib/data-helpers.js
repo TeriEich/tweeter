828085f2752229be2204b724345182a98f161f7e
Teri <teri.eich@gmail.com>
2018-12-13 20:52:27 +0000

    added Mongo db functionality

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+// const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -9,19 +9,21 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      // db.tweets.push(newTweet);
+      db.collection('tweets').insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      let sortedTweets = db.collection('tweets').find().sort({ "created_at": 1 }).toArray(callback);
+      // console.log(db.collection('tweets'));
+      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      // callback(null, db.tweets.sort(sortNewestFirst));
+      // callback(null, sortedTweets);
     }
 
+
   };
+  db.close();
 }
